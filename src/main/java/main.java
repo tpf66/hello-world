79af5aeeb9b45ce38c7d57e71e5cbc6a07669436
Leoanrdo Simoni <116296582+tpf66@users.noreply.github.com>
2022-10-28 19:38:18 +0200

    programma java

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
